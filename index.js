c2ab62a5201d87ad9c7f442bd1e406e0b2d86c39
codewithrabeeh <real.ray48@gmail.com>
2022-06-14 14:24:45 +0530

    intro to sessions

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 const session = require('express-session')
 
-app.use = session{}
+app.use(session({secret: 'thisisnotagoodsecret'}))
+
+app.get('/viewcount', (req, res) => {
+    if(req.session.count){
+        req.session.count += 1
+    } else {
+        req.session.count = 1
+    }
+    res.send(`You have viewed this page ${req.session.count} times`)
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
