679a2315e6e51e158c830f7661b95f4841308f73
codewithrabeeh <real.ray48@gmail.com>
2022-06-14 14:45:13 +0530

    more about express session

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const app = express()
 const session = require('express-session')
+/* Solving the express-session depricate warning */
+const sessionOptions = {secret: 'thisisnotagoodsecret', resave: false, saveUninitialized: false}
 
-app.use(session({secret: 'thisisnotagoodsecret'}))
+app.use(session(sessionOptions))
 
 app.get('/viewcount', (req, res) => {
     if(req.session.count){
         req.session.count += 1
-    } else {
+    } else { 
         req.session.count = 1
     }
     res.send(`You have viewed this page ${req.session.count} times`)
+})
+
+app.get('/register', (req, res) => {
+    const {username = 'Anonymous'} = req.query
+    req.session.username = username
+    res.redirect('/greet')
+})  
+
+app.get('/greet', (req, res) => {
+    const {username} = req.session
+    res.send(`Hey, there welcome back ${username}`) 
 })
 
 app.listen(3000, () => {
